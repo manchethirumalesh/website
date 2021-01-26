366a5391cd80d6b3cc47c5033ce065cd3e5f4bbd
manchethirumalesh <69119855+manchethirumalesh@users.noreply.github.com>
2021-01-26 16:10:34 +0530

    Add files via upload

---
+++ lyf/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+  }
+
+  public list = true;
+
+
+ clicked(event){
+   
+ 
+
+ }
+
+ myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+}
---
+++ lyf/index.js
@@ -0,0 +1,17 @@
+
+  list = true;
+
+
+  function clicked(event){
+    
+  
+  }
+ 
+  function myFunction() {
+   var x = document.getElementById("myTopnav");
+   if (x.className === "topnav") {
+     x.className += " responsive";
+   } else {
+     x.className = "topnav";
+   }
+ }
